d48cd637916ad53631da8bd58458122cda92c14a
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-05-10 18:31:21 -0400

    some corrections for forgotten parts

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -11,12 +11,19 @@ from textnode import (
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    # going to use a lot of if lines like above, each if line will need an "if 'delimiter' in X, then call the delimiter with
-    # that specific delimiter and text_type variable. the result needs to return a list of TextNodes. the .extend() method will be good here. 
    result = []
    for node in old_nodes:
+        if node.text_type != text_type_text: 
+            result.append(node)
+            continue
+        
+        
+        if len(node_text) % 2 == 0:
+            raise ValueError("Invalid MarkDown, formatted section not closed!"
+
         new_nodes = []
         node_text = copy.copy(node.text).split(delimiter)
+                         
         if node.text.startswith(delimiter):
             new_nodes.append(TextNode(node_text[0], node.text_type))
         for i in range (0, len(node_text)):
@@ -27,5 +34,4 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if node.text.endswith(delimiter):
             new_nodes.pop()
         result.extend(new_nodes)
-   print(result)
    return result
