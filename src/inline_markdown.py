f2b0bdd4b90e9e9e81bc8e163cbde39b285e1f32
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-05-22 17:24:38 -0400

    corrected functionality of split nodes image and link so that they properly function within the text_to_textnodes function.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -38,45 +38,61 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 def split_nodes_image(old_nodes):
     result = []
+
     for node in old_nodes:
-        if not re.search(r"!\[(.*?)\]\((.*?)\)", node.text):
-            result.append(node)
         if node.text_type != text_type_text:
             result.append(node)
+            continue
+
+        if not re.search(r"!\[(.*?)\]\((.*?)\)", node.text):
+            result.append(node)
+            continue
+
         images = extract_markdown_images(node.text)
-        contents = re.split(r'!|\)', node.text)
-        for item in contents:
-            if item == '':
+        if not images:
+            continue
+
+        contents = re.split(r'(!\[[^\]]*\]\([^\)]*\))', node.text)
+
+        for content in contents:
+            if content == '':
                 continue
-            if '[' in item:
-                result.append(TextNode(TEXT=images[0][0], TEXT_TYPE=text_type_image, URL=images[0][1]))
-                images.pop(0)
-                continue
-            if '[' not in item:
-                result.append(TextNode(TEXT=item, TEXT_TYPE=text_type_text))
-                continue
+
+            img_match = re.match(r'!\[(.*?)\]\((.*?)\)', content)
+            if img_match:
+                alt_text = img_match.group(1)
+                url = img_match.group(2)
+                result.append(TextNode(TEXT=alt_text, TEXT_TYPE= text_type_image, URL= url))
+            else:
+                result.append(TextNode(TEXT=content, TEXT_TYPE=text_type_text))
+
     return result
 
 
 def split_nodes_link(old_nodes):
     result = []
     for node in old_nodes: 
-        if not re.search(r"\[(.*?)\]\((.*?)\)", node.text):
-            result.append(node)
         if node.text_type != text_type_text:
             result.append(node)
-        links = extract_markdown_links(node.text)
-        contents = re.split(r'\[|\)', node.text)
-        for item in contents: 
-            if item == '':
+            continue
+
+        if not re.search(r"\[(.*?)\]\((.*?)\)", node.text):
+            result.append(node)
+            continue
+
+
+        contents = re.split(r'(\[[^\]]*\]\([^\)]*\))', node.text)
+        for content in contents: 
+            if content == '':
                 continue
-            if "]" in item:
-                result.append(TextNode(TEXT=links[0][0],TEXT_TYPE=text_type_link, URL=links[0][1]))
-                links.pop(0)
-                continue
-            if "]" not in item:
-                result.append(TextNode(TEXT=item, TEXT_TYPE=text_type_text))
-                continue
+
+            link_match = re.match(r'\[(.*?)\]\((.*?)\)', content)
+            if link_match:
+                link_text = link_match.group(1)
+                url = link_match.group(2)
+                result.append(TextNode(TEXT=link_text, TEXT_TYPE=text_type_link, URL=url))
+            else:
+                result.append(TextNode(TEXT=content, TEXT_TYPE=text_type_text))
     return result
 
 def extract_markdown_images(TEXT):
@@ -95,5 +111,9 @@ def extract_markdown_links(TEXT):
 
 def text_to_textnodes(text):
     start = TextNode(text, text_type_text)
-    result = split_nodes_link(split_nodes_image(split_nodes_delimiter(split_nodes_delimiter(split_nodes_delimiter(start, "**", text_type_bold), "*", text_type_italic), "`", text_type_code)))
-    return result
+    bold = split_nodes_delimiter([start], "**", text_type_bold)
+    italic = split_nodes_delimiter(bold, "*", text_type_italic)
+    code = split_nodes_delimiter(italic,"`", text_type_code)
+    images = split_nodes_image(code)
+    links = split_nodes_link(images)
+    return links
