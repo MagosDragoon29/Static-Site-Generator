337ff55d458738654ed029ecbf5cabccdcffc924
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-06-06 17:28:48 -0400

    added copy_files function to main.py. This function should, when called, delete all files in the public folder and then copy all files from a source directory to the public folder.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,37 @@
-print("hello world")
+import os
+import shutil
 from textnode import TextNode
+#page intentionally left blank
+
+#print("hello world")
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(root_dir, 'static')
+public_dir = os.path.join(root_dir, 'public')
+
 def main():
-    test_case = TextNode("This is a TextNode", "bold", "https://www.boot.dev/")
-    print(test_case)
+    #test_case = TextNode("This is a TextNode", "bold", "https://www.boot.dev/")
+    #print(test_case)
+    pass
+
+def copy_files(source_dir, sub_dir = None):
+    public_list = os.listdir(public_dir)
+    if not public_list:
+        shutil.rmtree(public_dir)
+        os.makedirs(public_dir)
+    all_items = os.listdir(source_dir)
+    destination_dir = public_dir
+    if sub_dir != None:
+        destination_dir = os.path.join(public_dir, sub_dir)
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+    for item in all_items:
+        item_path = os.path.join(source_dir, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, destination_dir)
+        elif os.path.isdir(item_path):
+            new_sub_dir = os.path.join(sub_dir or '', item)
+            copy_files(item_path, new_sub_dir)
+
+# page intentionally left blank    
 main()
